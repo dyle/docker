901fd3b5eb105ecddec0e803b9e087a11ca62f0f
Oliver Maurhart <oliver.maurhart@ait.ac.at>
2018-01-10 12:29:09 +0100

    makes cp commands in install.sh more expressive

--- install/install.sh
+++ install/install.sh
@@ -9,20 +9,20 @@ apt-get install -y apt-utils
 apt-get install -y sudo vim vim-common git screen bash-completion build-essential htop tree man-db
 
 # shell and user
-cp etc/bash.bashrc.local /etc/
-cp etc/vim/vimrc.local /etc/vim/
-cp etc/screenrc /etc/
-cp etc/bash.bashrc /tmp
+cp -rv etc/bash.bashrc.local /etc/
+cp -rv etc/vim/vimrc.local /etc/vim/
+cp -rv etc/screenrc /etc/
+cp -rv etc/bash.bashrc /tmp
 cat /tmp/bash.bashrc >> /root/.bashrc
 cat /tmp/bash.bashrc >> /home/${USERNAME}/.bashrc
 
 # git and vim
-cp home/.gitconfig /root/
-cp home/.gitconfig /home/${USERNAME}/
-cp home/.vimrc /root/
-cp home/.vim /root/.vim/
-cp home/.vimrc /home/${USERNAME}/
-cp home/.vim /home/${USERNAME}/.vim/
+cp -rv home/.gitconfig /root/
+cp -rv home/.gitconfig /home/${USERNAME}/
+cp -rv home/.vimrc /root/
+cp -rv home/.vim /root/.vim/
+cp -rv home/.vimrc /home/${USERNAME}/
+cp -rv home/.vim /home/${USERNAME}/.vim/
 
 # pull in Vundle vim packages
 git clone https://github.com/vim-scripts/ctrlp.vim.git /home/${USERNAME}/.vim/bundle/ctrlp.vim
@@ -31,7 +31,7 @@ git clone https://github.com/vim-airline/vim-airline.git /home/${USERNAME}/.vim/
 git clone https://github.com/vim-airline/vim-airline-themes.git /home/${USERNAME}/.vim/bundle/vim-airline-themes
 git clone https://github.com/airblade/vim-gitgutter.git /home/${USERNAME}/.vim/bundle/vim-gitgutter
 git clone https://github.com/VundleVim/Vundle.vim.git /home/${USERNAME}/.vim/bundle/Vundle.vim
-cp -r /home/${USERNAME}/.vim /root/
+cp -vr /home/${USERNAME}/.vim /root/
 
 # fix final group management and file permissions
 usermod --groups adm,sudo,audio,video,staff,games,users ${USERNAME}
